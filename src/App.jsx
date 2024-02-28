818db656195ebc05da7c0e31e67c57fa05aef961
Jp Keys <jpmusik29@gmail.com>
2024-02-28 09:06:19 -0500

    Adding UserInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header.jsx'
+import UserInput from './components/UserInput.jsx'
 
 function App() {
 
   return (
     <>
       <Header />
+      <UserInput />
     </>
   )
 }
