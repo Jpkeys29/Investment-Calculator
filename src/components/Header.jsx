48d6242d29b82775f7595c19f7fefb1dfd00c0d0
Jp Keys <jpmusik29@gmail.com>
2024-02-29 10:32:04 -0500

    Lifting State up from UserInput to App

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import logo from '../assets/investment-calculator-logo.png';
 
 export default function Header() {
     return (
+        
         <header id='header'>
             <h1>Investment Calculator</h1>
             <img src={logo} alt='Money bag' />
