3f3402c6bd3a4b4c8e4e35e9f04de2b9707a5e22
Jp Keys <jpmusik29@gmail.com>
2024-03-01 10:12:52 -0500

    Creating table and outputting results dynamically

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,5 +3,28 @@ import { calculateInvestmentResults } from "../util/investment";
 export default function Results({input}) {
     const resultsData = calculateInvestmentResults(input);
     console.log(resultsData)
-    return <p>Resultados</p>
+    return <table>
+        <thead>
+            <tr>
+                <th>Year</th>
+                <th>Investment Value</th>
+                <th>Interest (Year)</th>
+                <th>Total Interest</th>
+                <th>Invested Capital</th>
+            </tr>
+        </thead>
+        <tbody>
+            {resultsData.map(resultado => {
+                return (
+                    <tr key={resultado.year}>
+                        <td>{resultado.year}</td>
+                        <td>{resultado.valueEndOfYear}</td>
+                        <td>{resultado.interest}</td>
+                        {/* <td>{resultado.}</td>
+                        <td>{resultado.}</td> */}
+                    </tr>
+                ); 
+            })}
+        </tbody>
+    </table>
 }
